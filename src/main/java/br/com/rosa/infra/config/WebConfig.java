7162c9edda4adf69489436b8803c7b091f1fa2db
lucasemanoel385 <lucas.emanoelbastossilva@gmail.com>
2024-11-15 16:39:11 -0300

    Disable annotations @EnableWebMvc and add +1 ip domain

--- src/main/java/br/com/rosa/infra/config/WebConfig.java
+++ src/main/java/br/com/rosa/infra/config/WebConfig.java
@@ -2,6 +2,7 @@ package br.com.rosa.infra.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,16 +12,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 Isso é útil para habilitar CORS globalmente,
 mas não influencia diretamente o comportamento do Spring Security.*/
 @Configuration
-@EnableWebMvc
+//EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
 
 	@Value("${url.ip.static.domain}")
 	private String ipStatic;
 
+	@Value("${url.ip.static.domain.two}")
+	private String ipStaticTwo;
+
 	@Override
 	public void addCorsMappings(CorsRegistry resgistry) {
 		resgistry.addMapping("/**")
-								.allowedOrigins(ipStatic)
+								.allowedOrigins(ipStatic, ipStaticTwo)
 								.allowedHeaders("*")
 				                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
 								.allowCredentials(true);
